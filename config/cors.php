376206ae37d49b294b7e9d05dd2168c81757fc4a
fendnugraha <fend@fendnugrahas-MacBook-Air.local>
2025-07-22 17:40:34 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000', 'https://server.gsm-tronik.com', 'https://server.gemilangsuksesmuamalah.com')],
 
     'allowed_origins_patterns' => [],
 
